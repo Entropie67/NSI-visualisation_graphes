69eae49c9cfbdad244db97372430928708d57018
Olivier <olivier.elophe@gmail.com>
2022-03-14 23:55:44 +0100

    Initialisation

---
+++ script.js
@@ -0,0 +1,20 @@
+let nodes = new vis.DataSet([]);
+let edges = new vis.DataSet([]);
+
+let container = document.getElementById('graphe');
+
+let data = {
+    nodes: nodes,
+    edges: edges
+};
+let options = {};
+
+let network = new vis.Network(container, data, options);
+
+// -----------------------------------------------
+// -----------------------------------------------
+// -----NE touchez pas au dessus------------------
+// -----------------------------------------------
+// -----------------------------------------------
+
+
