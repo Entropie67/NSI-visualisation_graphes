9e0cc8f73d7ad27c4c570c4e629d7c651d0acaf1
Entropie67 <olivier.elophe@gmail.com>
2022-03-15 09:03:10 +0100

    Création de noeuds à partir d'une matrice d'adjacence

--- script.js
+++ script.js
@@ -17,4 +17,37 @@ let network = new vis.Network(container, data, options);
 // -----------------------------------------------
 // -----------------------------------------------
 
+let matrice = [
+    [0, 1, 0, 0],
+    [1, 1, 1, 1],
+    [0, 1, 0, 1],
+    [0, 1, 1, 0]
+]
+// Cette fonction 'transforme' une matrice m en graphe
+let matriceToGraphe = m => {
+    // Dans n on met la 'taille' de la matrice
+    let n = m.length;
+    // On fait une boucle pour créer n noeuds.
+    for (let i=0; i<n; i++){
+        nodes.add([
+            {
+                id: i,
+                label: (i+1).toString()
+            }
+        ]);
+    };
 
+    /*
+    edges.add(
+        [
+            {
+                from: 1,
+                to: 2,
+                label: "E1",
+            }
+        ]
+    );*/
+}
+
+// lancement de la fonction
+matriceToGraphe(matrice);
